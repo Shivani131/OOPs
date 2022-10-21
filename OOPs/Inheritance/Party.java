9626534a4bdb4834a7e0262b7050786926115b5e
Shivani131 <85801670+Shivani131@users.noreply.github.com>
2022-10-21 10:29:56 +0530

    Add files via upload

---
+++ OOPs/Inheritance/Party.java
@@ -0,0 +1,14 @@
+package party;
+import java.util.*;
+public class Party {
+  private int guests;
+  public int getGuests() {
+   return guests;
+  }
+  public void setGuests(int numGuests) {
+   guests = numGuests;
+  }
+  public void displayInvitation() {
+   System.out.println("Please come to my party!");
+  }
+}
